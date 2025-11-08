62033758a1ed4f5624d34a7649c87455b3ab3a0a
chereolisa <justincherechukwu@gmail.com>
2025-11-08 22:22:56 +0100

    task 7 updated

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,8 +2,10 @@ const arg = process.argv[2];
 const num = parseInt(arg, 10);
 
 if (Number.isInteger(num) && num > 0) {
-    for (let i = 0; i < num; i++) {
+    let i = 0;
+    while (i < num) {
         console.log("C is fun");
+        i++;
     }
 } else {
     console.log("Missing number of occurrences");
