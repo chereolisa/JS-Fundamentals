fa9ae0be5fedb356f85a160e4607f3834e9a11ad
chereolisa <justincherechukwu@gmail.com>
2025-11-08 22:26:22 +0100

    task 9 updated

--- 9-add.js
+++ 9-add.js
@@ -5,8 +5,4 @@ function add(a, b) {
 const first = parseInt(process.argv[2], 10);
 const second = parseInt(process.argv[3], 10);
 
-if (Number.isInteger(first) && Number.isInteger(second)) {
-    console.log(add(first, second));
-} else {
-    console.log("Missing or invalid arguments");
-}
+console.log(add(first, second));
